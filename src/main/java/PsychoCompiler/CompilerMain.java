10912079a32b0ba5c3ebbd1713a6d7823743ed56
熊伟伦 <330815461@qq.com>
2014-11-10 19:54:37 +0800

    init

---
+++ src/main/java/PsychoCompiler/CompilerMain.java
@@ -0,0 +1,10 @@
+package PsychoCompiler;
+
+/**
+ * Created by Azard on 2014/11/10.
+ */
+public class CompilerMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
