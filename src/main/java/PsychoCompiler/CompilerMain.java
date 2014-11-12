d21fff374125c351898e51aaa04f19f5a823109c
guoyanchang <752410335@qq.com>
2014-11-12 21:58:49 +0800

    test push

--- src/main/java/PsychoCompiler/CompilerMain.java
+++ src/main/java/PsychoCompiler/CompilerMain.java
@@ -5,6 +5,6 @@ import org.javacc.*;
 
 public class CompilerMain {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
